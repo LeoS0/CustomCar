5a43ecfe4700595073cd0f23e37c347b8bcabb35
LeoS0 <lenti11@live.se>
2021-09-12 16:42:24 +0200

    Update

---
+++ precache-manifest.8dadb0fda96d3013a19fe99bdc6374b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab19d035fc66f2315c5a",
+    "url": "/CustomCar/css/app.8056e7f6.css"
+  },
+  {
+    "revision": "d3ca1e39fd2fa0d17a1c8a03c6ee48c5",
+    "url": "/CustomCar/img/rotate.png"
+  },
+  {
+    "revision": "562417fdc142d49f24d19660926f1529",
+    "url": "/CustomCar/index.html"
+  },
+  {
+    "revision": "ab19d035fc66f2315c5a",
+    "url": "/CustomCar/js/app.b72a0a33.js"
+  },
+  {
+    "revision": "b705110f13698575ee06",
+    "url": "/CustomCar/js/chunk-vendors.76edcfcc.js"
+  },
+  {
+    "revision": "6cf35556693ce873131598eb804a0f93",
+    "url": "/CustomCar/json/data.json"
+  },
+  {
+    "revision": "7fc3cef0af61c3201b7fc1b308d36886",
+    "url": "/CustomCar/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CustomCar/robots.txt"
+  }
+]);
